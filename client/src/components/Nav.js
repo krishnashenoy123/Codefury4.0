27fe14e6b75ee0a7db2a06b6253d57d16b6f12b9
akshaj65 <jhonsonrozario13@gmail.com>
2021-10-02 13:52:33 +0530

    added a search bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import { GrSearch } from "react-icons/gr";
 function Nav() {
-    return(
+    return (
         <nav>
             <h3>dummy</h3>
+            <div id="search">
+                <input type="text" placeholder="Search" />
+                <i frame id="search-icon"><GrSearch /></i>
+            </div>
             <ul>
                 <li>
-                    <NavLink exact activeClassName="active" to = "/">
+                    <NavLink exact activeClassName="active" to="/">
                         Home
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink exact activeClassName="active" to = "/About">
+                    <NavLink exact activeClassName="active" to="/About">
                         About
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink exact activeClassName="active" to = "/Contact">
+                    <NavLink exact activeClassName="active" to="/Contact">
                         Contact
                     </NavLink>
                 </li>
